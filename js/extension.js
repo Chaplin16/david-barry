6be40d8aa69aa5ff260ff0e49854acb38229c825
Chaplin16 <chaplin16@orange.fr>
2021-10-28 23:33:42 +0200

    reglage du slick

---
+++ js/extension.js
@@ -0,0 +1,17 @@
+const extension = [
+    {
+        id: 1,
+        alt:"david barry maconnerie extension et beton",
+        photo:"images/slick-images/david-barry-maconnerie-beton.jpg"
+    },
+    {
+        id: 1,
+        alt:"david barry maconnerie mur d enceinte",
+        photo:"images/slick-images/david-barry-maconnerie-mur.jpg"
+    },
+    {
+        id: 1,
+        alt:"david barry maconnerie mur avec fenetre",
+        photo:"images/slick-images/david-barry-maconnerie-fenetre.jpg"
+    },
+]
