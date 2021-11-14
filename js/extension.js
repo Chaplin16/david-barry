aae2be9fd1f0b07dfc1d414873d603e9c3e5c02c
Chaplin16 <chaplin16@orange.fr>
2021-11-14 19:03:51 +0100

    rajout photos sur extension maison

--- js/extension.js
+++ js/extension.js
@@ -1,17 +1,37 @@
 const extension = [
     {
         id: 1,
-        alt:"david barry maconnerie extension et beton",
-        photo:"images/slick-images/david-barry-maconnerie-beton.jpg"
+        alt:"david barry maconnerie extension et beton aigues mortes",
+        photo:"images/slick-images/david-barry-maconnerie-maison-au-debut.jpg"
     },
     {
-        id: 1,
-        alt:"david barry maconnerie mur d enceinte",
+        id: 2,
+        alt:"david barry maconnerie mur d enceinte maison aigues mortes",
+        photo:"images/slick-images/david-barry-maconnerie-poutre.jpg"
+    },
+    {
+        id: 3,
+        alt:"david barry maconnerie mur maison aigues mortes",
         photo:"images/slick-images/david-barry-maconnerie-mur.jpg"
     },
     {
-        id: 1,
-        alt:"david barry maconnerie mur avec fenetre",
+        id: 4,
+        alt:"david barry maconnerie emplacement fenetre maison aigues mortes",
         photo:"images/slick-images/david-barry-maconnerie-fenetre.jpg"
+    },
+    {
+        id: 5,
+        alt:"david barry maconnerie mur exterieur fini maison aigues mortes",
+        photo:"images/slick-images/david-barry-maconnerie-mur-fini.jpg"
+    },
+    {
+        id: 5,
+        alt:"david barry maconnerie charpente bois maison aigues mortes",
+        photo:"images/slick-images/david-barry-maconnerie-charpente-bois.jpg"
+    },
+    {
+        id: 6,
+        alt:"david barry maconnerie charpente maison aigues mortes",
+        photo:"images/slick-images/david-barry-maconnerie-charpente.jpg"
     },
 ]
